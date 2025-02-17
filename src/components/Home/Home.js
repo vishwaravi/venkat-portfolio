21fd13647bcaa53d4fc19f0efa00c687ae3a1b0d
vishwaravi <vishwaman45@gmail.com>
2025-02-17 18:43:38 +0530

    second

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/avatar.png";
+import homeLogo from "../../Assets/avatar.webp";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
